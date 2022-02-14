2df912c4a9d693bba6f9e7d8a89e9a307591376f
Oscar Rénier <oscar.renier@gmail.com>
2022-02-14 03:41:14 -0800

    [ADD]: Logger appending logs to LogFile

--- src/Logger.java
+++ src/Logger.java
@@ -1,13 +1,21 @@
 package src;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Logger {
 
-    public static void log(Request request, Response response) {
+    public static void log(Request request, Response response, ServerConfig config) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("d/MMM/YYYY:H:m:s Z");
-        String message = String.format("%s - - [%s] %s %s -", request.getClientIp(), dateFormat.format(new Date()), String.format("\"%s %s %s\"", request.getMethod(), request.getUri(), request.getVersion()), response.getStatusCode());
+        String message = String.format("%s - - [%s] %s %s %d", request.getClientIp(), dateFormat.format(new Date()), String.format("\"%s %s %s\"", request.getMethod(), request.getUri(), request.getVersion()), response.getStatusCode(), response.getBody().getBytes().length);
         System.out.println(message);
+        // try {
+        //     Files.write(config.getLogFile(), message.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+        // } catch (IOException e) {
+        //     e.printStackTrace();
+        // }
     }
 }
