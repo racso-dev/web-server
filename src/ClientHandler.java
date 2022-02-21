280df9f208b48a170f630cf17328a65677b19062
Nicolas Antoine <nicolas.antauf@gmail.com>
2022-02-20 18:51:48 -0800

    [FIX]: merge errors

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -10,6 +10,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -20,12 +22,10 @@ public class ClientHandler implements Runnable {
   protected Socket clientSocket;
   private ServerConfig config;
   private String clientIp;
-  private HashMap<String, String> mimeMap;
 
-  public ClientHandler(Socket clientSocket, ServerConfig config, HashMap<String, String> mimeMap) {
+  public ClientHandler(Socket clientSocket, ServerConfig config) {
     this.clientSocket = clientSocket;
     this.config = config;
-    this.mimeMap = mimeMap;
     this.clientIp = clientSocket.getRemoteSocketAddress().toString().replace("/", "").split(":")[0];
   }
 
@@ -50,58 +50,58 @@ public class ClientHandler implements Runnable {
   //   Logger.log(request, response, config);
   // }
 
-  private void handleCgi(Request request, Response response) throws IOException {
-    String[] command = request.getUri().split("/");
-      String scriptPath = config.getScriptAlias().path + "/" + command[command.length - 1];
-      ProcessBuilder builder = new ProcessBuilder(scriptPath);
-      Map<String, String> env = builder.environment();
-      for (Map.Entry<String, String> entry : request.getHeaders().entrySet()) {
-        env.put("HTTP_" + entry.getKey(), entry.getValue());
-      }
-      env.put("QUERY_STRING", request.getQueryString());
-      env.put("SERVER_PROTOCOL", "HTTP/" + request.getVersion());
-      if (request.getMethod().equals("POST") || request.getMethod().equals("PUT")) {
-        builder.redirectInput(ProcessBuilder.Redirect.PIPE);
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(builder.start().getOutputStream()));
-        writer.write(request.getBody());
-        writer.flush();
-        writer.close();
-      }
-      builder.redirectErrorStream(true);
-      Process process = builder.start();
+  // private void handleCgi(Request request, Response response) throws IOException {
+  //   String[] command = request.getUri().split("/");
+  //     String scriptPath = config.getScriptAlias().path + "/" + command[command.length - 1];
+  //     ProcessBuilder builder = new ProcessBuilder(scriptPath);
+  //     Map<String, String> env = builder.environment();
+  //     for (Map.Entry<String, String> entry : request.getHeaders().entrySet()) {
+  //       env.put("HTTP_" + entry.getKey(), entry.getValue());
+  //     }
+  //     env.put("QUERY_STRING", request.getQueryString());
+  //     env.put("SERVER_PROTOCOL", "HTTP/" + request.getVersion());
+  //     if (request.getMethod().equals("POST") || request.getMethod().equals("PUT")) {
+  //       builder.redirectInput(ProcessBuilder.Redirect.PIPE);
+  //       BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(builder.start().getOutputStream()));
+  //       writer.write(request.getBody());
+  //       writer.flush();
+  //       writer.close();
+  //     }
+  //     builder.redirectErrorStream(true);
+  //     Process process = builder.start();
 
-      BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-      String line;
-      ArrayList<String> lines = new ArrayList<String>();
+  //     BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+  //     String line;
+  //     ArrayList<String> lines = new ArrayList<String>();
 
-      while ((line = reader.readLine()) != null)
-        lines.add(line);
-      response.setBody(String.join("\r\n", lines)).setStatusCode("200").send();
+  //     while ((line = reader.readLine()) != null)
+  //       lines.add(line);
+  //     response.setBody(String.join("\r\n", lines)).setStatusCode("200").send();
 
-  }
+  // }
 
-  private void handleFiles(Request request, Response response) {
-    File file = new File(config.getDocumentRoot() + request.getUri());
-      if (file.exists()) {
-        String fileContent = "";
-        try {
-          BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-          String line;
-          while ((line = reader.readLine()) != null) {
-            fileContent += line + "\r\n";
-          }
-          reader.close();
-        } catch (IOException e) {
-          e.printStackTrace();
-          response.setStatusCode("500").setBody("<h1>Internal Server Error</h1>").send();
-          return;
-        }
-        String extension = file.getName().split("\\.")[1];
-        response.setBody(fileContent).setStatusCode("200").setContentType(config.getMimeTypes().get(extension)).send();
-      } else {
-        response.setStatusCode("404").setBody("<h1>Not Found</h1>").send();
-      }
-  }
+  // private void handleFiles(Request request, Response response) {
+  //   File file = new File(config.getDocumentRoot() + request.getUri());
+  //     if (file.exists()) {
+  //       String fileContent = "";
+  //       try {
+  //         BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+  //         String line;
+  //         while ((line = reader.readLine()) != null) {
+  //           fileContent += line + "\r\n";
+  //         }
+  //         reader.close();
+  //       } catch (IOException e) {
+  //         e.printStackTrace();
+  //         response.setStatusCode("500").setBody("<h1>Internal Server Error</h1>").send();
+  //         return;
+  //       }
+  //       String extension = file.getName().split("\\.")[1];
+  //       response.setBody(fileContent).setStatusCode("200").setContentType(config.getMimeTypes().get(extension)).send();
+  //     } else {
+  //       response.setStatusCode("404").setBody("<h1>Not Found</h1>").send();
+  //     }
+  // }
 
   private void processRequest(Request request, Response response) throws IOException {
     if (request.getUri().startsWith(config.getScriptAlias().route)) {
@@ -111,7 +111,6 @@ public class ClientHandler implements Runnable {
       int bytesRead = 0;
       ProcessBuilder builder = new ProcessBuilder(path);
       builder.redirectErrorStream(true);
-      // Map<String, String> env = builder.environment();
       for (Map.Entry<String, String> entry : request.getHeaders().entrySet()) {
         builder.environment().put("HTTP_" + entry.getKey(), entry.getValue());
       }
@@ -130,7 +129,7 @@ public class ClientHandler implements Runnable {
       }
       response.setBody(result.getBytes())
         .setStatusCode(Response.statusCodes.get("OK"))
-        .setContentType(this.mimeMap.get("html"));
+        .setContentType(this.config.getMimeTypes().get("html"));
       Logger.log(request, response, config);
       response.send();
 
@@ -147,7 +146,7 @@ public class ClientHandler implements Runnable {
 
         response.setBody(bytes)
           .setStatusCode(Response.statusCodes.get("OK"))
-          .setContentType(this.mimeMap.get(extension));
+          .setContentType(this.config.getMimeTypes().get(extension));
       }
 
       Logger.log(request, response, config);
